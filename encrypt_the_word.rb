f9beec89a55e3e7b0f6c9128721614b001a676a7
Altervl <Altervl@yandex.ru>
2020-03-23 04:52:53 +0700

    add SHA2 method

--- encrypt_the_word.rb
+++ encrypt_the_word.rb
@@ -6,18 +6,21 @@ puts "Введите слово или фразу для шифрования:"
 message = STDIN.gets.chomp
 
 enc_mtd = ""
-until enc_mtd == "1" or enc_mtd == "2"
+until enc_mtd == "1" or enc_mtd == "2" or enc_mtd == "3"
   puts "Каким способом зашифровать?\n
   1 - MD5\n
-  2 - SHA1"
+  2 - SHA1\n
+  3 - SHA2"
 
   enc_mtd = STDIN.gets.chomp
 end
 
 if enc_mtd == "1"
   enc_message = Digest::MD5.hexdigest(message)
-else
+elsif enc_mtd == "2"
   enc_message = Digest::SHA1.hexdigest(message)
+else
+  enc_message = Digest::SHA2.hexdigest(message)
 end
 
 puts "Результат:\n
